92f036e7eb7dfa37a245e0646bbfd4487b013d6e
Alexandre Cordani <alcordani@gmail.com>
2015-11-10 07:08:44 +0100

    Automated commit at 2015-11-10 06:08:43 UTC by middleman-deploy 1.0.0

---
+++ javascripts/scroll-navbar.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//   // navbar transition jQuery script
+//   $(window).scroll(function(e){
+//     if ($(this).scrollTop() > 200) {
+//       $(".navbar").slideUp();
+//     }
+//     else {
+//       $(".navbar").slideDown();
+//     }
+//   });
+// });
